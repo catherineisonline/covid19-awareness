daeb999a8af5f4687ea7ed95f94275d121a06e3e
Catherine <63928803+catherinepng@users.noreply.github.com>
2020-09-30 00:12:21 +0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+  // ****** HAMBURGER MENU ******
+
+  const hamburger = document.getElementById('hamburger');
+  const navUl = document.getElementById('nav-ul');
+
+  hamburger.addEventListener('click', () => {
+  	navUl.classList.toggle('show'); 
+  });
+
+
+
+
+  
